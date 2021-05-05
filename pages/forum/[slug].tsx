c0aa1ac2f1e10e3b66531a290dbce47c33b572cd
fhs42724 <fhs42724@fh-salzburg.ac.at>
2021-05-05 17:47:22 +0200

    Satisfied linter

--- pages/forum/[slug].tsx
+++ pages/forum/[slug].tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { GetStaticProps, GetStaticPaths } from 'next';
 import { useRouter } from 'next/router';
 import { Params } from 'next/dist/next-server/server/router';
