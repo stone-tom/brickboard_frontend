e9233b99239a0d2869a28ae303832ea0520a48f8
Thomas Steiner <thomas.steiner.private@gmail.com>
2021-05-05 12:22:24 +0200

    removed session get

--- util/methods.ts
+++ util/methods.ts
@@ -10,18 +10,6 @@ export const get = async (url: string, options?: any) => {
   return res.json();
 };
 
-export const sessionGet = async (url: string, options?: any) => {
-  const res = await fetch(url, {
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    credentials: 'include',
-    ...options,
-  });
-  return res.json();
-};
-
 export const post = async (
   url: string,
   data?: any,
